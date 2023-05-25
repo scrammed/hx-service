9f6b998d7d0c01848bbf5849a88de3e0dea31722
scrammed <89460886@qq.com>
2023-05-25 16:39:43 +0800

    first commit

---
+++ api/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from steamship import Steamship
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+client = Steamship(workspace=os.environ.get('WORKSPACE'))
+generator = client.use_plugin(os.environ.get('STEAMSHIP_PLUGIN'))
+
+@app.route('/')
+def hello_world():
+    return 'Hello, huxian99'
+
+def test():
+    print()
+
+@app.route('/ai/gpt4')
+def hello_user():
+    q = request.args.get('q')
+    task = generator.generate(text=q)
+    task.wait()
+    return task.output.blocks[0].text
+
+if __name__ =="__main__":
+    app.run(debug=True, port=8086)
+
