e34409a5a266b901538bac3c57b0a37036332385
scrammed <89460886@qq.com>
2023-05-25 18:11:59 +0800

    error

--- api/app.py
+++ api/app.py
@@ -9,25 +9,25 @@ app = Flask(__name__)
 w = os.environ.get('WORKSPACE')
 # p = os.environ.get('STEAMSHIP_PLUGIN')
 
-m = 'null'
-if (isinstance(w, str)):
-    m = '是字符串'
-
 @app.route('/')
 def hello_world():
-    return 'Hello, huxian99' + m
+    return 'Hello, huxian99'
 
 def test():
     print()
 
 @app.route('/ai/gpt4')
 def hello_user():
-    client = Steamship(workspace=w)
-    generator = client.use_plugin('gpt-4')
-    q = request.args.get('q')
-    task = generator.generate(text=q)
-    task.wait()
-    return task.output.blocks[0].text
+    try:
+        client = Steamship(workspace=w)
+        generator = client.use_plugin('gpt-4')
+        q = request.args.get('q')
+        task = generator.generate(text=q)
+        task.wait()
+        return task.output.blocks[0].text
+    except Exception as e:
+        return e
+        
 
 if __name__ =="__main__":
     app.run(debug=True, port=8086)
